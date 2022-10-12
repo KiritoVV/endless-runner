870d2156d28663f85ad1cec75f31185a06c8ae86
KiritoVV <andyaguirrez098@gmail.com>
2022-10-12 17:13:15 +0100

    i have made a background for my game
    -parallaxing

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -25,24 +25,44 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-      //Create a `float` that will be equal to the players horizontal input
-      float movementValueX = 4.0f;
-      //Change the X velocity of the Rigidbody2D to be equal to the movement value
-      playerObject.velocity = new Vector2 (movementValueX, playerObject.velocity.y); 
+        //Create a `float` that will be equal to the players horizontal input
+        float movementValueX = 4.0f;
+        //Change the X velocity of the Rigidbody2D to be equal to the movement value
+        playerObject.velocity = new Vector2 (movementValueX, playerObject.velocity.y); 
 
-      //check to see if the ground check object is touching the ground
-      isOnGround = Physics2D.OverlapCircle(groundChecker.transform.position, 1.0f, whatIsGround);
+        //check to see if the ground check object is touching the ground
+        isOnGround = Physics2D.OverlapCircle(groundChecker.transform.position, 0.01f, whatIsGround);
 
-      if (Input.GetKeyDown(KeyCode.Space) && isOnGround == true && jump == true) 
-      {
-        playerObject.AddForce(new Vector2(0.0f, 600.0f));
-        jump = false;
-      }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if(isOnGround == true) 
+            {
+                playerObject.AddForce(new Vector2(0.0f, 600.0f));
+            } 
+            else if (jump == true) 
+            {
+                playerObject.velocity = new Vector2(playerObject.velocity.x, 0.0f);
+                playerObject.AddForce(new Vector2(0.0f, 50.0f));
+                jump = false;
+            }
+        }
 
-      if (Input.GetKeyDown(KeyCode.Space) && isOnGround == false && jump == false)
-      {
-        playerObject.AddForce(new Vector2(0.0f, 200.0f));
-        jump = true;
-      } 
+        if (isOnGround) {
+          jump = true;
+        }
     }
+        /*
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround == true && jump == true) 
+        {
+          playerObject.AddForce(new Vector2(0.0f, 600.0f));
+          jump = false;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround == false && jump == false)
+        {
+          playerObject.AddForce(new Vector2(0.0f, 200.0f));
+          jump = true;
+        } 
+        */
+
 }
